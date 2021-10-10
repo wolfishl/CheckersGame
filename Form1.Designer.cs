96c41ecffdac7a956c322ae9e70b772bc7a15c6d
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-10-10 16:26:31 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace CheckersGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.square1 = new System.Windows.Forms.Button();
+            this.square2 = new System.Windows.Forms.Button();
+            this.square4 = new System.Windows.Forms.Button();
+            this.square3 = new System.Windows.Forms.Button();
+            this.square8 = new System.Windows.Forms.Button();
+            this.square7 = new System.Windows.Forms.Button();
+            this.square6 = new System.Windows.Forms.Button();
+            this.square5 = new System.Windows.Forms.Button();
+            this.square15 = new System.Windows.Forms.Button();
+            this.square14 = new System.Windows.Forms.Button();
+            this.square13 = new System.Windows.Forms.Button();
+            this.square12 = new System.Windows.Forms.Button();
+            this.square11 = new System.Windows.Forms.Button();
+            this.square10 = new System.Windows.Forms.Button();
+            this.square9 = new System.Windows.Forms.Button();
+            this.square16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // square1
+            // 
+            this.square1.BackColor = System.Drawing.Color.Black;
+            this.square1.Location = new System.Drawing.Point(58, 49);
+            this.square1.Name = "square1";
+            this.square1.Size = new System.Drawing.Size(75, 75);
+            this.square1.TabIndex = 0;
+            this.square1.UseVisualStyleBackColor = false;
+            this.square1.Click += new System.EventHandler(this.square1_Click);
+            // 
+            // square2
+            // 
+            this.square2.BackColor = System.Drawing.Color.Red;
+            this.square2.Location = new System.Drawing.Point(131, 49);
+            this.square2.Name = "square2";
+            this.square2.Size = new System.Drawing.Size(75, 75);
+            this.square2.TabIndex = 1;
+            this.square2.UseVisualStyleBackColor = false;
+            // 
+            // square4
+            // 
+            this.square4.BackColor = System.Drawing.Color.Red;
+            this.square4.Location = new System.Drawing.Point(277, 49);
+            this.square4.Name = "square4";
+            this.square4.Size = new System.Drawing.Size(75, 75);
+            this.square4.TabIndex = 3;
+            this.square4.UseVisualStyleBackColor = false;
+            // 
+            // square3
+            // 
+            this.square3.BackColor = System.Drawing.Color.Black;
+            this.square3.Location = new System.Drawing.Point(204, 49);
+            this.square3.Name = "square3";
+            this.square3.Size = new System.Drawing.Size(75, 75);
+            this.square3.TabIndex = 2;
+            this.square3.UseVisualStyleBackColor = false;
+            // 
+            // square8
+            // 
+            this.square8.BackColor = System.Drawing.Color.Red;
+            this.square8.Location = new System.Drawing.Point(568, 49);
+            this.square8.Name = "square8";
+            this.square8.Size = new System.Drawing.Size(75, 75);
+            this.square8.TabIndex = 7;
+            this.square8.UseVisualStyleBackColor = false;
+            // 
+            // square7
+            // 
+            this.square7.BackColor = System.Drawing.Color.Black;
+            this.square7.Location = new System.Drawing.Point(495, 49);
+            this.square7.Name = "square7";
+            this.square7.Size = new System.Drawing.Size(75, 75);
+            this.square7.TabIndex = 6;
+            this.square7.UseVisualStyleBackColor = false;
+            // 
+            // square6
+            // 
+            this.square6.BackColor = System.Drawing.Color.Red;
+            this.square6.Location = new System.Drawing.Point(422, 49);
+            this.square6.Name = "square6";
+            this.square6.Size = new System.Drawing.Size(75, 75);
+            this.square6.TabIndex = 5;
+            this.square6.UseVisualStyleBackColor = false;
+            // 
+            // square5
+            // 
+            this.square5.BackColor = System.Drawing.Color.Black;
+            this.square5.Location = new System.Drawing.Point(349, 49);
+            this.square5.Name = "square5";
+            this.square5.Size = new System.Drawing.Size(75, 75);
+            this.square5.TabIndex = 4;
+            this.square5.UseVisualStyleBackColor = false;
+            // 
+            // square15
+            // 
+            this.square15.BackColor = System.Drawing.Color.Red;
+            this.square15.Location = new System.Drawing.Point(495, 122);
+            this.square15.Name = "square15";
+            this.square15.Size = new System.Drawing.Size(75, 75);
+            this.square15.TabIndex = 15;
+            this.square15.UseVisualStyleBackColor = false;
+            // 
+            // square14
+            // 
+            this.square14.BackColor = System.Drawing.Color.Black;
+            this.square14.Location = new System.Drawing.Point(422, 122);
+            this.square14.Name = "square14";
+            this.square14.Size = new System.Drawing.Size(75, 75);
+            this.square14.TabIndex = 14;
+            this.square14.UseVisualStyleBackColor = false;
+            // 
+            // square13
+            // 
+            this.square13.BackColor = System.Drawing.Color.Red;
+            this.square13.Location = new System.Drawing.Point(349, 122);
+            this.square13.Name = "square13";
+            this.square13.Size = new System.Drawing.Size(75, 75);
+            this.square13.TabIndex = 13;
+            this.square13.UseVisualStyleBackColor = false;
+            // 
+            // square12
+            // 
+            this.square12.BackColor = System.Drawing.Color.Black;
+            this.square12.Location = new System.Drawing.Point(276, 122);
+            this.square12.Name = "square12";
+            this.square12.Size = new System.Drawing.Size(75, 75);
+            this.square12.TabIndex = 12;
+            this.square12.UseVisualStyleBackColor = false;
+            // 
+            // square11
+            // 
+            this.square11.BackColor = System.Drawing.Color.Red;
+            this.square11.Location = new System.Drawing.Point(204, 122);
+            this.square11.Name = "square11";
+            this.square11.Size = new System.Drawing.Size(75, 75);
+            this.square11.TabIndex = 11;
+            this.square11.UseVisualStyleBackColor = false;
+            // 
+            // square10
+            // 
+            this.square10.BackColor = System.Drawing.Color.Black;
+            this.square10.Location = new System.Drawing.Point(131, 122);
+            this.square10.Name = "square10";
+            this.square10.Size = new System.Drawing.Size(75, 75);
+            this.square10.TabIndex = 10;
+            this.square10.UseVisualStyleBackColor = false;
+            // 
+            // square9
+            // 
+            this.square9.BackColor = System.Drawing.Color.Red;
+            this.square9.Location = new System.Drawing.Point(58, 122);
+            this.square9.Name = "square9";
+            this.square9.Size = new System.Drawing.Size(75, 75);
+            this.square9.TabIndex = 9;
+            this.square9.UseVisualStyleBackColor = false;
+            // 
+            // square16
+            // 
+            this.square16.BackColor = System.Drawing.Color.Black;
+            this.square16.Location = new System.Drawing.Point(568, 122);
+            this.square16.Name = "square16";
+            this.square16.Size = new System.Drawing.Size(75, 75);
+            this.square16.TabIndex = 8;
+            this.square16.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 561);
+            this.Controls.Add(this.square15);
+            this.Controls.Add(this.square14);
+            this.Controls.Add(this.square13);
+            this.Controls.Add(this.square12);
+            this.Controls.Add(this.square11);
+            this.Controls.Add(this.square10);
+            this.Controls.Add(this.square9);
+            this.Controls.Add(this.square16);
+            this.Controls.Add(this.square8);
+            this.Controls.Add(this.square7);
+            this.Controls.Add(this.square6);
+            this.Controls.Add(this.square5);
+            this.Controls.Add(this.square4);
+            this.Controls.Add(this.square3);
+            this.Controls.Add(this.square2);
+            this.Controls.Add(this.square1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button square1;
+        private System.Windows.Forms.Button square2;
+        private System.Windows.Forms.Button square4;
+        private System.Windows.Forms.Button square3;
+        private System.Windows.Forms.Button square8;
+        private System.Windows.Forms.Button square7;
+        private System.Windows.Forms.Button square6;
+        private System.Windows.Forms.Button square5;
+        private System.Windows.Forms.Button square15;
+        private System.Windows.Forms.Button square14;
+        private System.Windows.Forms.Button square13;
+        private System.Windows.Forms.Button square12;
+        private System.Windows.Forms.Button square11;
+        private System.Windows.Forms.Button square10;
+        private System.Windows.Forms.Button square9;
+        private System.Windows.Forms.Button square16;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CheckersGame
+{
+    public partial class Form1 : Form
+    {
+
+        Button[,] board; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            board = new Button [2, 8] { { square1 , square2, square3, square4, square5, square6, square7, square8 },
+            { square9, square10, square11, square12, square13, square14, square15, square16 } };
+        }
+
+        public void DisplayButtonLocation(object sender)
+        {
+            Button btn = (Button)sender;
+            var name = btn.Name;
+
+            for (int x = 0; x < board.Length; x++)
+            {
+                for (int y = 0; y < board.Length; y++)
+                {
+                    if (board[x, y].Name == name)
+                    {
+                        MessageBox.Show($"Location of button is [{x + 1},{y + 1}]");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void square1_Click(object sender, EventArgs e)
+        {
+            DisplayButtonLocation(sender); 
+        }
+    }
+}
